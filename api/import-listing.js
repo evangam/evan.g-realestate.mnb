4d4cb5c66fa30ec543ea5d18ac42d8479d70eb7c
evangam <evangam1122@gmail.com>
2026-09-14 10:48:40 +1000

    Support Homely description imports

--- api/import-listing.js
+++ api/import-listing.js
@@ -5,6 +5,8 @@ const ALLOWED_HOSTS = [
   'www.domain.com.au',
   'raineandhorne.com.au',
   'www.raineandhorne.com.au',
+  'homely.com.au',
+  'www.homely.com.au',
 ];
 
 function decode(value = '') {
